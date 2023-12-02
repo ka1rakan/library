f03463ead0becf4fe1b3e98653a993521013e17c
ka1rakan <hsuhankaraca@gmail.com>
2023-12-02 17:02:43 +0300

    add buttons to card

--- script.js
+++ script.js
@@ -5,21 +5,49 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
-const it = new Book("It", "Stephen King", 1061, "not read yet")
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read");
 
-const myLibrary = [theHobbit, it];
+
+const myLibrary = [theHobbit];
+
 
 function addBookToLibrary(newBook){
+    const readSwitch = document.createElement("button");
     myLibrary.push(newBook);
     const card = document.createElement("div");
     card.className = "card";
     const ul = document.createElement("ul");
     for(let key in newBook){
-        const li = document.createElement("li");
-        li.innerText = `${newBook[key]}`
-        ul.appendChild(li)
+        if(key!=="read"){
+            const li = document.createElement("li");
+            li.innerText = newBook[key]
+            ul.appendChild(li)
+        }else if(key==="read"){
+            readSwitch.innerText = newBook[key];
+            if (readSwitch.innerText=="read"){
+                readSwitch.classList = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded button-read";
+            }else if(readSwitch.innerText=="not read"){
+                readSwitch.classList = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded button-not-read"
+            }
+            readSwitch.addEventListener("click",(e)=>{
+                if(readSwitch.innerText == "read"){
+                    readSwitch.innerText = "not read";
+                    readSwitch.classList = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded button-not-read"
+                }else if(readSwitch.innerText == "not read"){
+                    readSwitch.innerText = "read";
+                    readSwitch.classList = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded button-read"
+                }
+            })
+            ul.appendChild(readSwitch)
+        }
     }
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove"
+    removeButton.classList = "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded remove-button"
+    removeButton.addEventListener("click",(e)=>{
+        e.target.parentNode.parentNode.remove();
+    })
+    ul.appendChild(removeButton);
     card.appendChild(ul);
     container.appendChild(card);
 }
@@ -27,17 +55,8 @@ function addBookToLibrary(newBook){
 
 const container = document.querySelector(".container");
 for (let index in myLibrary){
-    const book = myLibrary[index];
-    const card = document.createElement("div");
-    card.className = "card";
-    const ul = document.createElement("ul")
-    for(let key in myLibrary[index]){
-        const li = document.createElement("li");
-        li.innerText = `${key}: ${book[key]}`
-        ul.appendChild(li)
-    }
-    card.appendChild(ul)
-    container.appendChild(card)
+    addBookToLibrary(myLibrary[index])
+    
 }
 
 
@@ -52,7 +71,7 @@ openModalBtn.addEventListener("click", ()=>{
     dialog.showModal();
 })
 
-const newBook={};
+const newBook= new Book();
 modalInput.forEach((input)=>{
     input.addEventListener("change",(e)=>{
         newBook[e.target.id] = e.target.value;
@@ -63,7 +82,9 @@ modalInput.forEach((input)=>{
 addBookBtn.addEventListener("click",(e)=>{
     if(newBook.title && newBook.author && newBook.pages){
         if(newBook.read==undefined){
-            newBook.read = "not read yet"
+            newBook.read = "not read"
+        }else{
+            newBook.read = "read"
         }
         addBookToLibrary(newBook);
     }
