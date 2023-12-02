a86f2a46cdd2fcc9487b9f677c5241e5a6e5b2c2
ka1rakan <hsuhankaraca@gmail.com>
2023-12-02 16:00:52 +0300

    add add-book logic

--- script.js
+++ script.js
@@ -5,37 +5,69 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "have not read yet");
-const it = new Book("It", "Stephen King", 1061, "have not read yet")
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
+const it = new Book("It", "Stephen King", 1061, "not read yet")
 
 const myLibrary = [theHobbit, it];
 
-function addBookToLibrary(){
-    
+function addBookToLibrary(newBook){
+    myLibrary.push(newBook);
+    const card = document.createElement("div");
+    card.className = "card";
+    const ul = document.createElement("ul");
+    for(let key in newBook){
+        const li = document.createElement("li");
+        li.innerText = `${newBook[key]}`
+        ul.appendChild(li)
+    }
+    card.appendChild(ul);
+    container.appendChild(card);
 }
 
-const addBookBtn = document.querySelector("#add-book-btn")
-const dialog = document.querySelector("dialog");
-const modalInput = document.querySelectorAll("dialog input");
-addBookBtn.addEventListener("click", ()=>{
-    dialog.showModal();
-})
-const obj={};
-modalInput.forEach((input)=>{
-    input.addEventListener("change",(e)=>{
-        obj[e.target.id] = e.target.value;
-    })
-})
 
 const container = document.querySelector(".container");
 for (let index in myLibrary){
     const book = myLibrary[index];
     const card = document.createElement("div");
     card.className = "card";
+    const ul = document.createElement("ul")
     for(let key in myLibrary[index]){
         const li = document.createElement("li");
         li.innerText = `${key}: ${book[key]}`
-        card.appendChild(li)
+        ul.appendChild(li)
     }
+    card.appendChild(ul)
     container.appendChild(card)
 }
+
+
+const openModalBtn = document.querySelector("#add-book-btn")
+const dialog = document.querySelector("dialog");
+const modalInput = document.querySelectorAll("dialog input");
+const addBookBtn = document.querySelector("#confirm-dialog");
+const addBookForm = document.querySelector("dialog form")
+
+openModalBtn.addEventListener("click", ()=>{
+    addBookForm.reset();
+    dialog.showModal();
+})
+
+const newBook={};
+modalInput.forEach((input)=>{
+    input.addEventListener("change",(e)=>{
+        newBook[e.target.id] = e.target.value;
+    })
+})
+
+
+addBookBtn.addEventListener("click",(e)=>{
+    if(newBook.title && newBook.author && newBook.pages){
+        if(newBook.read==undefined){
+            newBook.read = "not read yet"
+        }
+        addBookToLibrary(newBook);
+    }
+})
+
+
+
