3637aeafcf36f89e080bbd309e22924d37d1f7d8
ka1rakan <hsuhankaraca@gmail.com>
2023-12-16 01:50:20 -0800

    replace function with class implementation

--- script.js
+++ script.js
@@ -1,62 +1,71 @@
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages; 
-    this.read = read;
-}
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read");
-
-
-const myLibrary = [theHobbit];
-
-
-function addBookToLibrary(newBook){
-    const readSwitch = document.createElement("button");
-    myLibrary.push(newBook);
-    const card = document.createElement("div");
-    card.className = "card";
-    const ul = document.createElement("ul");
-    for(let key in newBook){
-        if(key!=="read"){
-            const li = document.createElement("li");
-            li.innerText = newBook[key]
-            ul.appendChild(li)
-        }else if(key==="read"){
-            readSwitch.innerText = newBook[key];
-            if (readSwitch.innerText=="read"){
+    addSwitchBtn(){
+        const readSwitch = document.createElement("button");
+        readSwitch.innerText = this.read ? "read" : "not read";
+            if(this.read){
                 readSwitch.classList = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded button-read";
-            }else if(readSwitch.innerText=="not read"){
+            }else if(!this.read){
                 readSwitch.classList = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded button-not-read"
             }
-            readSwitch.addEventListener("click",(e)=>{
-                if(readSwitch.innerText == "read"){
+            readSwitch.addEventListener("click", (e) => {
+                if(this.read){
+                    this.read = !this.read;
                     readSwitch.innerText = "not read";
                     readSwitch.classList = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded button-not-read"
-                }else if(readSwitch.innerText == "not read"){
+                }else{
+                    this.read = !this.read;
                     readSwitch.innerText = "read";
                     readSwitch.classList = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded button-read"
                 }
             })
-            ul.appendChild(readSwitch)
-        }
+        return readSwitch
     }
-    const removeButton = document.createElement("button");
-    removeButton.innerText = "Remove"
-    removeButton.classList = "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded remove-button"
-    removeButton.addEventListener("click",(e)=>{
-        e.target.parentNode.parentNode.remove();
-    })
-    ul.appendChild(removeButton);
-    card.appendChild(ul);
-    container.appendChild(card);
+
+    addRemoveBtn(){
+        const removeButton = document.createElement("button");
+        removeButton.innerText = "Remove"
+        removeButton.classList = "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded remove-button"
+        removeButton.addEventListener("click",(e)=>{
+            e.target.parentNode.parentNode.remove();
+        })
+        return removeButton;
+    }
+
+    addToLibrary(){
+        myLibrary.push(this);
+        const card = document.createElement("div");
+        card.className = "card";
+        const ul = document.createElement("ul");
+        for(let key in this){
+            if(key!=="read"){
+                const li = document.createElement("li");
+                li.innerText = this[key];
+                ul.appendChild(li);
+            }else if(key==="read"){
+                ul.appendChild(this.addSwitchBtn())
+            }
+        }
+        ul.appendChild(this.addRemoveBtn());
+        card.appendChild(ul);
+        container.appendChild(card);
+    }
+
 }
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read");
+
+const myLibrary = [theHobbit];
 
 
 const container = document.querySelector(".container");
 for (let index in myLibrary){
-    addBookToLibrary(myLibrary[index])
-    
+    myLibrary[index].addToLibrary()
 }
 
 
@@ -71,7 +80,7 @@ openModalBtn.addEventListener("click", ()=>{
     dialog.showModal();
 })
 
-const newBook= new Book();
+const newBook = new Book();
 modalInput.forEach((input)=>{
     input.addEventListener("change",(e)=>{
         newBook[e.target.id] = e.target.value;
@@ -86,7 +95,7 @@ addBookBtn.addEventListener("click",(e)=>{
         }else{
             newBook.read = "read"
         }
-        addBookToLibrary(newBook);
+        newBook.addToLibrary();
     }
 })
 
