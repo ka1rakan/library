b7aa03c420e9b3eaf0d61665205a4926669fc619
ka1rakan <kairaskaraca@gmail.com>
2024-10-11 07:47:57 +1100

    script: Add card content on click

--- script.js
+++ script.js
@@ -28,7 +28,24 @@ function toggleBookRead(id) {
 
 addBookBtn.addEventListener('click', () => {
   const book1 = new Book('test', 'testAuth', 100, true);
-  const node = document.createElement('div');
-  node.innerText = book1.title;
-  container.appendChild(node)
+  const card = document.createElement('div');
+  card.classList = ['card'];
+  const title = document.createElement('h2');
+  title.classList = ['title'];
+  title.innerText = book1.title;
+  const ul = document.createElement('ul');
+  const li1 = document.createElement('li');
+  li1.innerText = `author: ${book1.author}`;
+  ul.appendChild(li1);
+  const li2 = document.createElement('li');
+  li2.innerText = `pages: ${book1.pages}`;
+  ul.appendChild(li2);
+  const li3 = document.createElement('li');
+  li3.innerText = book1.read;
+  ul.appendChild(li3);
+  card.appendChild(title);
+  card.appendChild(ul);
+
+
+  container.appendChild(card)
 })
