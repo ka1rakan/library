7b24872f215cf2aff6695638980a06a21e846c7d
ka1rakan <kairaskaraca@gmail.com>
2024-10-11 08:46:24 +1100

    add modal dialog add book panel

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const addBookBtn = document.querySelector('.addBook');
 const container = document.querySelector('.container')
+const modal = document.querySelector('dialog');
 
 const library = [];
 let maxId = 0;
@@ -25,8 +26,29 @@ function toggleBookRead(id) {
   const book = library.find(book => book.id === id);
   book.read = !book.read;
 }
+const book1 = new Book('test', 'testAuth', 100, true);
+const card = document.createElement('div');
+card.classList = ['card'];
+const title = document.createElement('h2');
+title.classList = ['title'];
+title.innerText = book1.title;
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = `author: ${book1.author}`;
+ul.appendChild(li1);
+const li2 = document.createElement('li');
+li2.innerText = `pages: ${book1.pages}`;
+ul.appendChild(li2);
+const li3 = document.createElement('li');
+li3.innerText = book1.read;
+ul.appendChild(li3);
+card.appendChild(title);
+card.appendChild(ul);
+container.appendChild(card);
 
 addBookBtn.addEventListener('click', () => {
+  modal.showModal();
+  modal.classList('dialog-open')
   const book1 = new Book('test', 'testAuth', 100, true);
   const card = document.createElement('div');
   card.classList = ['card'];
